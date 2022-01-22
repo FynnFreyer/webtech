04e8922c9b20c93a77f08213e9c9e90e60abb6b6
younes <younes.abdelwadoud@outlook.de>
2022-01-22 02:21:09 +0100

    timeline filter fixes

--- js/map.js
+++ js/map.js
@@ -49,7 +49,7 @@ function updateMap(countries) {
         "color": "#000000",
         "weight": 2
     };
-    mymap.clear();
+    mymap.remove();
 
     let visitedCountries = [];
     for (let i = 0; i < countries.length; i++) {
